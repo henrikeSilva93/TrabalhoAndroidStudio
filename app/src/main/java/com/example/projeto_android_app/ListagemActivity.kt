a47c58bebb6573791d9a7ede3b346c3c3b3e6fec
aquila Henrique <aquilahenrique.silva@gmail.com>
2021-11-25 19:25:01 -0300

    added list activity

---
+++ app/src/main/java/com/example/projeto_android_app/ListagemActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projeto_android_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListagemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listagem)
+    }
+}
