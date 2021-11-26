c7faa7d19335740ee22b4c1f9f015771f5468754
aquila Henrique <aquilahenrique.silva@gmail.com>
2021-11-26 16:08:56 -0300

    Adicionado navegação entre telas

--- app/src/main/java/com/example/projeto_android_app/ListagemActivity.kt
+++ app/src/main/java/com/example/projeto_android_app/ListagemActivity.kt
@@ -7,5 +7,6 @@ class ListagemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listagem)
+        setTitle("Receitas da Vovo")
     }
 }
--- app/src/main/java/com/example/projeto_android_app/myApp.kt
+++ app/src/main/java/com/example/projeto_android_app/myApp.kt
@@ -1,12 +1,24 @@
 package com.example.projeto_android_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class myApp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTitle("Receitas da vovó")
         setContentView(R.layout.myapp)
+        val btnLogin = findViewById<Button>(R.id.bt_login)
+        btnLogin.setOnClickListener() {
+            irParaTelaLogin()
+        }
+
+    }
+
+    private fun irParaTelaLogin() {
+        val telaLogin = Intent(this, ListagemActivity::class.java)
+        startActivity(telaLogin)
     }
 }
